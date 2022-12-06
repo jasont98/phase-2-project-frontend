29e8047c8840575264cac8c59fb4ee8bc59e5072
Ian Holsteen <ianholsteen@gmail.com>
2022-12-06 10:08:40 -0800

    ian add weathercard

---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,11 @@
+function WeatherCard({ weather }) {
+    return (
+        <div>
+            <h1>{weather.name}</h1>
+            <h1>{}</h1>
+        </div>
+    )
+
+}
+
+export default WeatherCard;
