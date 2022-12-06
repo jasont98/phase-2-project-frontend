3c62074b8476d221d5663e23e559f3259968beb7
Ian Holsteen <ianholsteen@gmail.com>
2022-12-06 15:25:43 -0800

    search

---
+++ src/Components/Search.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Search({ onSubmitQuery }) {
+    
+    const [city, setCity] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onSubmitQuery(city);
+      }
+    
+      function handleChange(event) {
+        setCity(event.target.value);
+      }    
+
+
+    return (
+        <form onSubmit={handleSubmit}>Search for a city or zipcode: 
+            <input value={city} onChange={handleChange} />
+            <button>🔎</button>
+        </form>
+    )
+}
+
+export default Search;
