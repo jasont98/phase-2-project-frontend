ca9b465a49fb78a8e9acde710f406debb92f9030
Ian Holsteen <ianholsteen@gmail.com>
2022-12-06 15:28:55 -0800

    ian changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,17 @@
 // import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import '../App.css';
-import WeatherCard from './weatherCard';
+import WeatherCard from './WeatherCard';
 import Search from './Search';
 
 function App() {
 
   const [weather, setWeather] = useState([]);
-
+  const [city, setCity] = useState('denver');
   const APIKey = '963f1edba44b1dcf82fe895ef811cf6a';
-
-  const [city, setCity] = useState('denver')
-  
   const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  // console.log(city)
 
-  console.log(city)
   useEffect(() => {
     fetch(queryURL)
       .then((r)=>r.json())
@@ -22,9 +19,8 @@ function App() {
       (setWeather(weather))
     );
   }, [city])
+  // console.log(weather)
   
-  console.log(weather)
-
   return (
     <div>
       <Search onSubmitQuery={setCity} />
