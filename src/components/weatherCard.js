8c3e7297f71adc9f3087398e93d67cded1ee5b12
Ian Holsteen <ianholsteen@gmail.com>
2022-12-06 09:35:40 -0800

    messing with api endpoints

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -2,7 +2,7 @@ function WeatherCard({ weather }) {
     return (
         <div>
             <h1>{weather.name}</h1>
-            <h1>{weather.weather[0].main}</h1>
+            <h1>{}</h1>
         </div>
     )
 
