3ceade2a03026507877651412819c74be02d0ae2
Hoang Tran <jashoang2583@gmail.com>
2022-12-07 15:00:51 -0800

    whole lotta styles

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,11 +9,24 @@ const About = () => {
     text-align: center;
     color: darkcyan;
     padding-top: 150px;
+    padding-bottom: 50px;
+    /* background-image: url("../public/sunset.jpg") */
   }
 
   body {
     color: darkcyan; 
     font-family: Tahoma;
+    /* background-color: darkslateblue; */
+    background-image: url("./snowy-landscape.jpg");
+    image-resolution: 200dpi; 
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-size: cover;
+    background-position: center
   }
 `
 
@@ -31,6 +44,7 @@ const About = () => {
      <div>
        <h4>A simple app that allows the user to search thru different weathers of cities, and add those cities to a watchlist</h4>
        <p>Created by: William, Ian and Jason</p>
+      <img></img>
       </div>
     </>
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SnowFall from 'react-snowfall';
 import {createGlobalStyle, ThemeProvider} from "styled-components";
 import { keyframes } from 'styled-components';
@@ -6,6 +6,24 @@ import styled from 'styled-components';
 import WeatherCard from './WeatherCard'
 
 const Home = ({ weather1, weather2, weather3 }) => {
+
+  const images = ["./snow-city.jpg", "./rainy-day.jpg", "./sunny-field.jpg", "./clear-night.jpg"]
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        if(currentIndex === images.length - 1) {
+            setCurrentIndex(0);
+        } 
+        else {
+             setCurrentIndex(currentIndex + 1);
+        }
+    }, 2000)
+    
+    return () => clearInterval(intervalId);
+}, [])
+
   const GlobalStyle = createGlobalStyle`
   .About {
     text-align: center;
@@ -16,6 +34,28 @@ const Home = ({ weather1, weather2, weather3 }) => {
   body {
     color: darkcyan; 
     font-family: Tahoma;
+    background-color: darkslateblue;
+    background-image: url("./snow-city.jpg"); 
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-size: cover;
+    background-position: center
+    // flexbox or css grid
+    // mediaQuery
+  }
+  .cardDifs {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-size: cover;
+    background-position: center
   }
 `
 
@@ -32,11 +72,10 @@ const rotate = keyframes`
 // Here we create a component that will rotate everything we pass in over two seconds
 const Rotate = styled.div`
   display: inline-block;
-  animation: ${rotate} 2s linear infinite;
+  animation: ${rotate} 3s linear infinite;
   padding: 2rem 1rem;
   font-size: 4.2rem;
 `;
-
 
   return (
     <>
@@ -48,6 +87,9 @@ const Rotate = styled.div`
     <Rotate> 🥶 </Rotate>
       <br></br>
        <h2 style={{color: 'darkcyan', fontFamily: 'Tahoma'} }>Home</h2> 
+    </div>
+    <div className="cardDifs">
+      <img src={images[currentIndex]} />
     </div>
     
     <div className="flexbox-container">
